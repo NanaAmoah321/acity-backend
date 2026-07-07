74c59e76cd6c6fc2ece382cc181b7631e68c0d6d
NanaAmoah321 <njstyles690@gmail.com>
2026-07-07 15:06:44 +0000

    stuff

--- workers/emailWorker.js
+++ workers/emailWorker.js
@@ -7,19 +7,57 @@ async function processEmailQueue(){
 
     try{
 
-        const jobs =
-        await pool.query(
+        const jobs = await pool.query(
+`
+UPDATE email_queue
+SET status = 'processing'
+WHERE id IN (
 
-            `
-            SELECT *
+    SELECT id
 
-            FROM email_queue
+    FROM email_queue
 
-            WHERE processed = FALSE
+    WHERE
 
-            ORDER BY created_at ASC
-            `
-        );
+        status = 'pending'
+
+    ORDER BY created_at ASC
+
+    LIMIT 500
+
+)
+
+RETURNING *;
+`
+);
+
+const groupedJobs = {};
+
+jobs.rows.forEach(job => {
+
+    const key = `${job.user_id}_${job.seller_id}_${job.type}`;
+
+    if(!groupedJobs[key]){
+
+        groupedJobs[key] = [];
+
+    }
+
+    groupedJobs[key].push(job);
+
+});
+
+for(const key in groupedJobs){
+
+    const jobs = groupedJobs[key];
+
+    console.log(
+
+        `Processing ${jobs.length} jobs for ${key}`
+
+    );
+
+}
 
         console.log(
 
@@ -39,7 +77,8 @@ setInterval(
 
     processEmailQueue,
 
-    10 * 60 * 1000
+    /*10 * 60 * 1000*/
+    10000
 
 );
 
